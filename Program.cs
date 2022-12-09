558ead14ecda2dcdb639050dcdfad04e63d2eaaa
BuchowiczA <80336991+BuchowiczA@users.noreply.github.com>
2022-12-09 19:27:04 +0100

    Dodanie aktualnego stanu projektu

---
+++ Program.cs
@@ -0,0 +1,12 @@
+namespace Sudoku
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new SudokuForm());
+        }
+    }
+}
