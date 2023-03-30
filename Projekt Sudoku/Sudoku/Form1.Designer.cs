bbaf9d8ea985bdaf9917af46a8ae257261eb7859
BuchowiczA <polpolish@gmail.com>
2023-03-30 09:41:39 +0200

    finałowa wersja programu

---
+++ Projekt Sudoku/Sudoku/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Sudoku
+{
+    partial class SudokuForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            label1 = new Label();
+            beginnerLevel = new RadioButton();
+            intermediateLevel = new RadioButton();
+            advancedLevel = new RadioButton();
+            checkButton = new Button();
+            ngButton = new Button();
+            clearButton = new Button();
+            resButton = new Button();
+            hintButton = new Button();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(beginnerLevel);
+            panel1.Controls.Add(intermediateLevel);
+            panel1.Controls.Add(advancedLevel);
+            panel1.Controls.Add(checkButton);
+            panel1.Location = new Point(14, 16);
+            panel1.Margin = new Padding(3, 4, 3, 4);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(617, 840);
+            panel1.TabIndex = 0;
+            panel1.Paint += panel1_Paint;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 20F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(-2, 765);
+            label1.Name = "label1";
+            label1.RightToLeft = RightToLeft.No;
+            label1.Size = new Size(300, 46);
+            label1.TabIndex = 7;
+            label1.Text = "Poziom trudności";
+            label1.Click += label1_Click;
+            // 
+            // beginnerLevel
+            // 
+            beginnerLevel.AutoSize = true;
+            beginnerLevel.Location = new Point(3, 815);
+            beginnerLevel.Margin = new Padding(3, 4, 3, 4);
+            beginnerLevel.Name = "beginnerLevel";
+            beginnerLevel.Size = new Size(114, 24);
+            beginnerLevel.TabIndex = 6;
+            beginnerLevel.TabStop = true;
+            beginnerLevel.Text = "Początkujący";
+            beginnerLevel.UseVisualStyleBackColor = true;
+            // 
+            // intermediateLevel
+            // 
+            intermediateLevel.AutoSize = true;
+            intermediateLevel.Location = new Point(123, 815);
+            intermediateLevel.Margin = new Padding(3, 4, 3, 4);
+            intermediateLevel.Name = "intermediateLevel";
+            intermediateLevel.Size = new Size(180, 24);
+            intermediateLevel.TabIndex = 5;
+            intermediateLevel.TabStop = true;
+            intermediateLevel.Text = "Średniozaawansowany";
+            intermediateLevel.UseVisualStyleBackColor = true;
+            // 
+            // advancedLevel
+            // 
+            advancedLevel.AutoSize = true;
+            advancedLevel.Location = new Point(309, 816);
+            advancedLevel.Margin = new Padding(3, 4, 3, 4);
+            advancedLevel.Name = "advancedLevel";
+            advancedLevel.Size = new Size(131, 24);
+            advancedLevel.TabIndex = 4;
+            advancedLevel.TabStop = true;
+            advancedLevel.Text = "Zaawansowany";
+            advancedLevel.UseVisualStyleBackColor = true;
+            // 
+            // checkButton
+            // 
+            checkButton.Location = new Point(457, 743);
+            checkButton.Margin = new Padding(3, 4, 3, 4);
+            checkButton.Name = "checkButton";
+            checkButton.Size = new Size(160, 93);
+            checkButton.TabIndex = 2;
+            checkButton.Text = "Sprawdź";
+            checkButton.UseVisualStyleBackColor = true;
+            checkButton.Click += checkButton_Click;
+            // 
+            // ngButton
+            // 
+            ngButton.Location = new Point(178, 917);
+            ngButton.Margin = new Padding(3, 4, 3, 4);
+            ngButton.Name = "ngButton";
+            ngButton.Size = new Size(123, 93);
+            ngButton.TabIndex = 1;
+            ngButton.Text = "Nowa gra";
+            ngButton.UseVisualStyleBackColor = true;
+            ngButton.Click += ngButton_Click;
+            // 
+            // clearButton
+            // 
+            clearButton.Location = new Point(12, 917);
+            clearButton.Margin = new Padding(3, 4, 3, 4);
+            clearButton.Name = "clearButton";
+            clearButton.Size = new Size(123, 93);
+            clearButton.TabIndex = 3;
+            clearButton.Text = "Wyczyść";
+            clearButton.UseVisualStyleBackColor = true;
+            clearButton.Click += clearButton_Click;
+            // 
+            // resButton
+            // 
+            resButton.Location = new Point(344, 917);
+            resButton.Margin = new Padding(3, 4, 3, 4);
+            resButton.Name = "resButton";
+            resButton.Size = new Size(123, 93);
+            resButton.TabIndex = 4;
+            resButton.Text = "Pokaż rozwiązanie";
+            resButton.UseVisualStyleBackColor = true;
+            resButton.Click += resButton_Click;
+            // 
+            // hintButton
+            // 
+            hintButton.Location = new Point(510, 917);
+            hintButton.Margin = new Padding(3, 4, 3, 4);
+            hintButton.Name = "hintButton";
+            hintButton.Size = new Size(123, 93);
+            hintButton.TabIndex = 5;
+            hintButton.Text = "Pokaż podpowiedź";
+            hintButton.UseVisualStyleBackColor = true;
+            hintButton.Click += hintButton_Click;
+            // 
+            // SudokuForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(645, 1055);
+            Controls.Add(hintButton);
+            Controls.Add(resButton);
+            Controls.Add(clearButton);
+            Controls.Add(ngButton);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "SudokuForm";
+            Text = "Sudoku";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Button ngButton;
+        private Button checkButton;
+        private Button clearButton;
+        private RadioButton beginnerLevel;
+        private RadioButton intermediateLevel;
+        private RadioButton advancedLevel;
+        private Label label1;
+        private Button resButton;
+        private Button hintButton;
+    }
+}
---
+++ Projekt Sudoku/Sudoku/Form1.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Windows.Forms;
+
+namespace Sudoku
+{
+    public partial class SudokuForm : Form
+    {
+        public SudokuForm()
+        {
+            InitializeComponent();
+
+            createCells();
+
+            startNewGame();
+        }
+
+        SudokuCell[,] cells = new SudokuCell[9, 9];
+
+        /// <summary>
+        /// Metoda tworzaca siatke gry 9x9 
+        /// </summary>
+        private void createCells()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    cells[i, j] = new SudokuCell();
+                    cells[i, j].Font = new Font(SystemFonts.DefaultFont.FontFamily, 30);
+                    cells[i, j].Size = new Size(60, 60);
+                    cells[i, j].ForeColor = SystemColors.ControlDarkDark;
+                    cells[i, j].Location = new Point(i * 60, j * 60);
+                    cells[i, j].BackColor = ((i / 3) + (j / 3)) % 2 == 0 ? SystemColors.Control : Color.LightGray;
+                    cells[i, j].FlatStyle = FlatStyle.Flat;
+                    cells[i, j].X = i;
+                    cells[i, j].Y = j;
+
+                    cells[i, j].KeyPress += cell_keyPressed;
+
+                    panel1.Controls.Add(cells[i, j]);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Metoda sluzaca do wstawiania cyfr do komorek
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void cell_keyPressed(object sender, KeyPressEventArgs e)
+        {
+            var cell = sender as SudokuCell;
+
+            // Jezeli komórka zostala wygenerowana przez gre
+            if (cell.IsLocked)
+                return;
+
+            int value;
+
+            if (int.TryParse(e.KeyChar.ToString(), out value))
+            {
+                if (value == 0)
+                    cell.Clear();
+                else
+                    cell.Text = value.ToString();
+                cell.ForeColor = SystemColors.ControlDarkDark;
+            }
+        }
+
+        /// <summary>
+        /// Metoda wykonywana przy rozpoczynaniu nowej gry, wykonuje sie przy nacisnieciu przycisku nowej gry
+        /// </summary>
+        private void startNewGame()
+        {
+            loadValues();
+
+            var hintsCount = 0;
+
+            if (beginnerLevel.Checked)
+                hintsCount = 45;
+            else if (intermediateLevel.Checked)
+                hintsCount = 30;
+            else if (advancedLevel.Checked)
+                hintsCount = 15;
+
+            showRandomValuesHints(hintsCount);
+        }
+
+        /// <summary>
+        /// Metoda czysci siatke i laduje nowe wartosci do komorek
+        /// </summary>
+        private void loadValues()
+        {
+            foreach (var cell in cells)
+            {
+                cell.Value = 0;
+                cell.Clear();
+            }
+
+            findValueForNextCell(0, -1);
+        }
+
+        Random random = new Random();
+
+
+        /// <summary>
+        /// Funkcja uzupelniajaca wszystkie komorki cyframi, ktore nie sa widoczne dla gracza
+        /// Dzieki temu podczas rozwiazywania sudoku poszczegolne komorki jedynie zmieniaja swoj typ na tekst aby zostaly wyswietone
+        /// </summary>
+        /// <param name="i"></param>
+        /// <param name="j"></param>
+        /// <returns></returns>
+        private bool findValueForNextCell(int i, int j)
+        {
+            if (++j > 8)
+            {
+                j = 0;
+
+                if (++i > 8)
+                    return true;
+            }
+
+            var value = 0;
+            var numsLeft = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            do
+            {
+                if (numsLeft.Count < 1)
+                {
+                    cells[i, j].Value = 0;
+                    return false;
+                }
+
+                value = numsLeft[random.Next(0, numsLeft.Count)];
+                cells[i, j].Value = value;
+
+                numsLeft.Remove(value);
+            }
+            while (!isValidNumber(value, i, j) || !findValueForNextCell(i, j));
+
+            return true;
+        }
+
+        /// <summary>
+        /// Metoda sprawdzajaca poprawnosc wstawionych cyfr
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        private bool isValidNumber(int value, int x, int y)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                if (i != y && cells[x, i].Value == value)
+                    return false;
+
+                if (i != x && cells[i, y].Value == value)
+                    return false;
+            }
+
+            for (int i = x - (x % 3); i < x - (x % 3) + 3; i++)
+            {
+                for (int j = y - (y % 3); j < y - (y % 3) + 3; j++)
+                {
+                    if (i != x && j != y && cells[i, j].Value == value)
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Funkcja uzupelniajaca poszczegolna ilosc komorek na podstawie poziomu trudnosci
+        /// </summary>
+        /// <param name="hintsCount"></param>
+        private void showRandomValuesHints(int hintsCount)
+        {
+            for (int i = 0; i < hintsCount; i++)
+            {
+                var rX = random.Next(9);
+                var rY = random.Next(9);
+
+                cells[rX, rY].Text = cells[rX, rY].Value.ToString();
+                cells[rX, rY].ForeColor = Color.Black;
+                cells[rX, rY].IsLocked = true;
+            }
+        }
+
+        /// <summary>
+        /// Funkcja uzupeniajaca wszystkie komorki sudoku poprawnymi wartosciami
+        /// </summary>
+        private void showCorrectValues()
+        {
+            for (int i = 0; i <= 8; i++)
+            {
+                for (int j = 0; j <= 8; j++)
+                {
+                    cells[i, j].Text = cells[i, j].Value.ToString();
+                    cells[i, j].IsLocked = true;
+                    cells[i, j].ForeColor = Color.Black;
+                }
+            }
+            MessageBox.Show("Pola zostały wypełnione poprawnymi wartościami.");
+        }
+
+        /// <summary>
+        /// Funkcja uzupelniajaca jedna losowa komorke
+        /// </summary>
+        private void showHint()
+        {
+            Random rnd = new Random();
+            int i = rnd.Next(0, 9);
+            int j = rnd.Next(0, 9);
+            if (cells[i, j].IsLocked == false)
+            {
+                cells[i, j].Text = cells[i, j].Value.ToString();
+                cells[i, j].IsLocked = true;
+                cells[i, j].ForeColor = Color.Black;
+                MessageBox.Show("Uzupełniono jedno pole.");
+            }
+            else
+            {
+                showHint();
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Przycisk sprawdzajacy, czy sudoku zostalo poprawnie rozwiazane
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void checkButton_Click(object sender, EventArgs e)
+        {
+            var wrongCells = new List<SudokuCell>();
+
+            foreach (var cell in cells)
+            {
+                if (!string.Equals(cell.Value.ToString(), cell.Text))
+                {
+                    wrongCells.Add(cell);
+                }
+            }
+
+            if (wrongCells.Any())
+            {
+                wrongCells.ForEach(x => x.ForeColor = Color.Red);
+                MessageBox.Show("Jest parę błędów.");
+            }
+            else
+            {
+                MessageBox.Show("Udało Ci się rozwiązać zagadkę!");
+            }
+        }
+
+        /// <summary>
+        /// Przycisk czyszczacy plansze
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            foreach (var cell in cells)
+            {
+                if (cell.IsLocked == false)
+                    cell.Clear();
+            }
+        }
+
+        /// <summary>
+        /// Przycisk rozpoczynajacy nowa gre
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ngButton_Click(object sender, EventArgs e)
+        {
+            startNewGame();
+        }
+
+        /// <summary>
+        /// Przycisk rozwiazujacy cala zagadke
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void resButton_Click(object sender, EventArgs e)
+        {
+            showCorrectValues();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Przycisk uzupelniajacy jedna komorke
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void hintButton_Click(object sender, EventArgs e)
+        {
+            showHint();
+        }
+    }
+}
